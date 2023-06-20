cb8d55a182b55652ccb6ac652a16bd8b9d0a8c68
Haris Kamal <61567146+HarisK03@users.noreply.github.com>
2023-06-20 00:23:48 -0400

    Fix type annotations

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -14,15 +14,15 @@ class Solution:
     Note: A more optimal (linear time) solution is possible using Manacher's algorithm
     """
 
-    def longestPalindrome(self, text):
+    def longestPalindrome(self, s: str) -> str:
         longest_start, longest_end = 0, 1
 
-        for index in range(len(text)):
+        for index in range(len(s)):
             for left, right in (index, index), (index, index + 1):
-                while 0 <= left and right < len(text) and text[left] == text[right]:
+                while 0 <= left and right < len(s) and s[left] == s[right]:
                     left -= 1
                     right += 1
                 if (right - left) - 1 > longest_end - longest_start:
                     longest_start, longest_end = left + 1, right
 
-        return text[longest_start:longest_end]
+        return s[longest_start:longest_end]
