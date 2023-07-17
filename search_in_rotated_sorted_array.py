64514effe8231714eb48fda97fa393fdef5eb713
kamalhar <hariskamal03@gmail.com>
2023-07-17 19:43:37 -0400

    Added LeetCode 33 Python solution

---
+++ search_in_rotated_sorted_array.py
@@ -0,0 +1,40 @@
+"""
+LeetCode 33 - Search in Rotated Sorted Array
+Author: Haris Kamal
+Video Walkthrough: https://www.youtube.com/watch?v=RcGo-qjr5UQ
+"""
+
+from typing import List
+
+
+class Solution:
+    """
+    Solution 1: Binary Search
+    Time Complexity: O(log(n))
+    Space Complexity: O(1)
+    """
+
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            # left partition
+            elif nums[l] <= nums[mid]:
+                if nums[l] <= target <= nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+
+            # right partition
+            else:
+                if nums[mid] <= target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+        return -1
